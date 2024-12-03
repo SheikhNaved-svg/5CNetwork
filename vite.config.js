6888835a57a19938a54c38c57102f61c96baae44
sheikh naved <iamnavedsheikh@gmail.com>
2024-12-03 11:54:07 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allow connections from other devices
-    port: 5173,      // Default port for Vite
-  },
+ 
  
 })
